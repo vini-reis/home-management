2d4af0920f575bbd0f7b323d28a4f16391908704
Vinícius Reis <vinecreis@gmail.com>
2022-06-01 20:25:56 -0300

    Setting OneTapUI sign in on LoginFragment.kt

--- app/src/main/java/com/reis/vinicius/homemanagement/MainActivity.kt
+++ app/src/main/java/com/reis/vinicius/homemanagement/MainActivity.kt
@@ -1,37 +1,20 @@
 package com.reis.vinicius.homemanagement
 
-import android.app.Activity
-import android.content.Intent
-import android.content.IntentSender
 import android.os.Bundle
-import android.util.Log
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import androidx.navigation.fragment.findNavController
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.identity.SignInClient
-import com.google.android.gms.common.api.ApiException
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
-import com.reis.vinicius.homemanagement.data.auth.SignInEnum
 import com.reis.vinicius.homemanagement.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-    private lateinit var auth: FirebaseAuth
-    private lateinit var oneTapClient: SignInClient
-    private lateinit var signInRequest: BeginSignInRequest
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, true)
@@ -42,30 +25,9 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        val navController = getNavController()
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-
-        auth = Firebase.auth
-
-        if (auth.currentUser != null)
-            goToHome()
-
-        oneTapClient = Identity.getSignInClient(this)
-        signInRequest = BeginSignInRequest.builder()
-            .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
-                .setSupported(true)
-                .build())
-            .setGoogleIdTokenRequestOptions(
-                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                    .setSupported(true)
-                    .setServerClientId(getString(R.string.web_client_id))
-                    .setFilterByAuthorizedAccounts(true)
-                    .build())
-            .setAutoSelectEnabled(true)
-            .build()
-
-        launchGoogleOneTap()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -89,51 +51,6 @@ class MainActivity : AppCompatActivity() {
                 || super.onSupportNavigateUp()
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        when (resultCode) {
-            Activity.RESULT_OK -> {
-                try {
-                    val googleCredential = oneTapClient
-                        .getSignInCredentialFromIntent(data)
-                    val idToken = googleCredential.googleIdToken
-
-                    if (idToken == null)
-                        Log.e("AUTH", "No ID Token!");
-                    else {
-                        val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
-                        auth.signInWithCredential(firebaseCredential)
-                            .addOnCompleteListener { result ->
-                                if (result.isSuccessful)
-                                    goToHome()
-                                else
-                                    Log.e("AUTH", result.exception?.localizedMessage ?: "")
-                            }
-                    }
-                } catch (e: ApiException) {
-                    Log.e("AUTH", "Failed to get sign in result: $e")
-                }
-            }
-        }
-    }
-
-    private fun launchGoogleOneTap(){
-        oneTapClient.beginSignIn(signInRequest)
-            .addOnSuccessListener {
-                try {
-                    startIntentSenderForResult(it.pendingIntent.intentSender, SignInEnum.GOOGLE.code,
-                        null, 0, 0, 0, null)
-                } catch (e: IntentSender.SendIntentException){
-                    Log.e("AUTH", e.localizedMessage)
-                }
-            }
-            .addOnFailureListener { e ->
-                Log.e("AUTH", e.localizedMessage)
-            }
-    }
-
-    private fun goToHome() {
-        findNavController(R.id.nav_host_fragment_content_main).navigate(LoginFragmentDirections.login())
-    }
+    private fun getNavController() = (supportFragmentManager.findFragmentById(
+        R.id.nav_host_fragment_content_main) as NavHostFragment).navController
 }
